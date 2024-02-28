2f0819b2a50783edb0f11f9d59348dff7e03b174
Prajjwal-dev <prajjwal.maharjan2013@gmail.com>
2024-02-28 18:09:31 +0545

    Structure_changes

--- Structure5.cpp
+++ Structure5.cpp
@@ -12,26 +12,26 @@ struct Employee {
 };
 
 int main() {
-    struct Employee employees[NUM_EMPLOYEES];
+    struct Employee e[NUM_EMPLOYEES];
 
     // Input data for each employee
     for (int i = 0; i < NUM_EMPLOYEES; i++) {
         printf("Enter details for employee %d:\n", i + 1);
         printf("Enter employee ID: ");
-        scanf("%d", &employees[i].emp_id);
+        scanf("%d", &e[i].emp_id);
         printf("Enter employee name: ");
-        scanf("%s", employees[i].emp_name);
+        scanf("%s", e[i].emp_name);
         printf("Enter employee salary: ");
-        scanf("%f", &employees[i].emp_salary);
+        scanf("%f", &e[i].emp_salary);
     }
 
     // Display employee information whose name starts with 'D'
     printf("\nEmployee information whose name starts with 'D':\n");
     for (int i = 0; i < NUM_EMPLOYEES; i++) {
-        if (employees[i].emp_name[0] == 'D') {
-            printf("Employee ID: %d\n", employees[i].emp_id);
-            printf("Employee Name: %s\n", employees[i].emp_name);
-            printf("Employee Salary: %.2f\n", employees[i].emp_salary);
+        if (e[i].emp_name[0] == 'D') {
+            printf("Employee ID: %d\n", e[i].emp_id);
+            printf("Employee Name: %s\n", e[i].emp_name);
+            printf("Employee Salary: %.2f\n", e[i].emp_salary);
             printf("\n");
         }
     }
